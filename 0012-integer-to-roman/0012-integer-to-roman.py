08c8c9fb28f34d0fab3ac2439aae252a1a0ebd0b
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-02-10 14:35:39 +0300

    Update 0012-integer-to-roman.py

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,19 +1,17 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman={
-            1:"I",
-            4:"IV",
-            5:"V",
-            9:"IX",
-            10:"X",
-            40:"XL",
-            50:"L",
-            90:"XC",
-            1:"I",
-            1:"I",
-            1:"I",
-            1:"I",
+        roman = {1000:"M",900:"CM",500:"D",400:"CD",100:"C",
+         90:"XC",50:"L",40:"XL",10:"X",9:"IX",
+         5:"V",4:"IV",1:"I"}
+        res = []
+        for key , val in roman.items():
+            x = num//key
+            num %= key
+            res.append(val*x)
+            if num == 0:
+                break
+        # print(res)
+        return ''.join(res)
 
-        }
 
         
