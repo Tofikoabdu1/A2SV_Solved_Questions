fd2bf69647ee6c05659a8ca7377954446b4cbf46
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-02-10 14:32:01 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,19 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman={
+            1:"I",
+            4:"IV",
+            5:"V",
+            9:"IX",
+            10:"X",
+            40:"XL",
+            50:"L",
+            90:"XC",
+            1:"I",
+            1:"I",
+            1:"I",
+            1:"I",
+
+        }
+
+        
