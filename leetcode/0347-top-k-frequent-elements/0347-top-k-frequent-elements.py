82c047b0188d54e3b0c2c4837645f332f4f15ff1
Tofik Abdulkerim <165275773+Tofikoabdu1@users.noreply.github.com>
2026-04-08 18:22:51 +0300

    Add solution for Top K Frequent Elements

---
+++ leetcode/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,7 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+        val = list(count.values())
+        val.sort()
+        max_val = set(val[-k:])
+        return [key for key , val in count.items() if val in max_val ]
